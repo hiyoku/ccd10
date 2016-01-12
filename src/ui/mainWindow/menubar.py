2381ca9783b65743a2ada1f982372660f84c9fbe
hiyoku <hiyoku@hotmail.co.jp>
2016-01-12 10:33:08 -0200

    Commited on PyCharm

--- src/ui/mainWindow/menubar.py
+++ src/ui/mainWindow/menubar.py
@@ -1,18 +1,28 @@
-from PyQt5.QtWidgets import QAction, qApp
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction, qApp
 
 
 def init_menu(self):
-    # Creting the Menu Bar
+    # Creating the Menu Bar
     menubar = self.menuBar()
     add_to_menu(menubar, action_close(self))
 
 
 def action_close(self):
     # Creating the button to close the application
-    aexit = QAction(QIcon('icons/exit.png'), "&Exit",self , shortcut="Ctrl+Q")
+    aexit = QAction(QIcon('icons\exit.png'), "&Exit", self)
+    aexit.setShortcut("Ctrl+Q")
     aexit.setStatusTip("Exit Application")
-    aexit.triggered.connect(qApp.quit)
+
+    # Define a new Signal called triggerActionClose without arguments
+    self.trigger_aexit = pyqtSignal()
+
+    # Connect the trigger_aexit to a Slot
+    self.trigger_aexit.connect(qApp.quit)
+
+    # Emit the Signal
+    self.trigger_aexit.emit()
 
     return aexit, "&File"
 
