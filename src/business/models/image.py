b35881cf5dc579c073eba54cdbafe98970686a3d
Plínio Pereira <pliniojr@gmail.com>
2016-12-20 19:59:56 -0200

    Try except get_level sbigdriver :goberserk:

--- src/business/models/image.py
+++ src/business/models/image.py
@@ -1,8 +1,10 @@
 
 class Image:
-    def __init__(self, path, pngname, fitsname, date, hour):
+    def __init__(self, path, pngname, fitsname, date, hour, getlevel1, getlevel2):
         self.path = path
         self.png_name = pngname
         self.fits_name = fitsname
         self.date = date
         self.hour = hour
+        self.getlevel1 = getlevel1
+        self.getlevel2 = getlevel2
